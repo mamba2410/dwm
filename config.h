a1d3d57cf2fc701bacd3f44eded075d38f14e68d
mamba2410 <callummcgregor54@gmail.com>
2020-05-10 12:21:31 +0100

    Added full gaps

--- config.h
+++ config.h
@@ -3,6 +3,7 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int gappx		= 8;		/* gap size */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
@@ -108,6 +109,9 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_e,      quit,           {0} },
+	{ MODKEY,						XK_minus,  setgaps,		   {.i = -1}},
+	{ MODKEY,						XK_equal,  setgaps,		   {.i = +1}},
+	{ MODKEY|ShiftMask,				XK_equal,  setgaps,		   {.i = 0}},
 };
 
 /* button definitions */
